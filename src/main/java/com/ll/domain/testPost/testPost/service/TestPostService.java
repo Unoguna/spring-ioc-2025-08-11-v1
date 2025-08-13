909f6db861fda14c37b40ae530ca55099f5aaaa5
Jeongho Kim <kimjungho.kr@gmail.com>
2025-08-13 10:36:48 +0900

    feat: 빈 생성시 하드코딩으로 service와 repository빈 생성

--- src/main/java/com/ll/domain/testPost/testPost/service/TestPostService.java
+++ src/main/java/com/ll/domain/testPost/testPost/service/TestPostService.java
@@ -3,7 +3,7 @@ package com.ll.domain.testPost.testPost.service;
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
+@RequiredArgsConstructor //final이나 @NonNull이 붙은 필드를 대상으로 자동으로 생성자를 만들어주는 기능
 public class TestPostService {
     private final TestPostRepository testPostRepository;
 }
--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,22 @@
 package com.ll.framework.ioc;
 
-public class ApplicationContext {
-    public ApplicationContext() {
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestPostService;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApplicationContext {
+    private final Map<String, Object> beans = new HashMap<>();
+
+    public ApplicationContext() {
+        TestPostRepository repo = new TestPostRepository(); // 먼저 레포지토리 생성
+        TestPostService service = new TestPostService(repo); // 의존성 주입
+        beans.put("testPostRepository", repo);
+        beans.put("testPostService", service);
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        return (T) beans.get(beanName);
     }
 }
